20209549b77cb5a3dc39060280eaf0e660c01421
Mamun <mamunmiah193gsc@gmail.com>
2022-08-16 07:48:03 +0600

    Adding a ViewModel

--- app/src/main/java/com/example/android/trackmysleepquality/MainActivity.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/MainActivity.kt
@@ -39,7 +39,6 @@ import androidx.appcompat.app.AppCompatActivity
 
 /**
  * This main activity is just a container for our fragments,
- * where the real action is.
  */
 class MainActivity : AppCompatActivity() {
 
